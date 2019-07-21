3d66c49fb513d436c1e4316ad0459f1d260fcf86
Saugata Paul <saugata.paul1010@gmail.com>
2019-07-22 01:35:35 +0530

    Fully cleaned and updated code for all 7 models.

--- Backend/core_app.py
+++ Backend/core_app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 import keras.backend as K
 from datetime import datetime as dt
 import numpy as np
-import cv2 import *
+import cv2
 from cv2 import resize, INTER_AREA
 import uuid
 from PIL import Image
@@ -12,6 +12,15 @@ import tempfile
 from keras.models import load_model
 import imageio
 from keras.preprocessing import image
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout, Flatten
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+import keras
+from keras.applications import vgg16
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.imagenet_utils import decode_predictions
 
 
 def resize_image_oct(image):
@@ -26,6 +35,45 @@ def resize_image_pnm(image):
         resized_image = cv2.cvtColor(resized_image,cv2.COLOR_GRAY2RGB) #Convert to RGB
     return resized_image
 
+def convert_results(string):
+  name = string.replace("_"," ")
+  name = name.replace("-"," ")
+  name = name.title()
+  return name
+
+def load_vgg16_model():
+  input_shape = (224, 224, 3)
+
+  #Instantiate an empty model
+  vgg_model = Sequential([
+  Conv2D(64, (3, 3), input_shape=input_shape, padding='same', activation='relu'),
+  Conv2D(64, (3, 3), activation='relu', padding='same'),
+  MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+  Conv2D(128, (3, 3), activation='relu', padding='same'),
+  Conv2D(128, (3, 3), activation='relu', padding='same',),
+  MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+  Conv2D(256, (3, 3), activation='relu', padding='same',),
+  Conv2D(256, (3, 3), activation='relu', padding='same',),
+  Conv2D(256, (3, 3), activation='relu', padding='same',),
+  MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+  Conv2D(512, (3, 3), activation='relu', padding='same',),
+  Conv2D(512, (3, 3), activation='relu', padding='same',),
+  Conv2D(512, (3, 3), activation='relu', padding='same',),
+  MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+  Conv2D(512, (3, 3), activation='relu', padding='same',),
+  Conv2D(512, (3, 3), activation='relu', padding='same',),
+  Conv2D(512, (3, 3), activation='relu', padding='same',),
+  MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+  Flatten(),
+  Dense(4096, activation='relu'),
+  Dense(4096, activation='relu'),
+  Dense(1000, activation='softmax')
+  ])
+
+  vgg_model.load_weights("weights/vgg16.h5")
+
+  return vgg_model
+
 app = Flask(__name__)
 CORS(app)
 
@@ -38,11 +86,6 @@ def index():
   
   if request.method == "POST":
     type_ = request.form.get("type", None)
-
-    #print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!: ",request.files)
-    print("22222222222222222222222222222222222222222222222222",len(request.files))
-
-    print(type_)
     data = None
     final_json = []
     if 'img' in request.files:
@@ -51,13 +94,6 @@ def index():
       file_.save(name)
       print("[DEBUG: %s]"%dt.now(),name)
 
-      """
-      test_image=imageio.imread(name)
-      test_image=cv2.resize(test_image, (128,128), interpolation = cv2.INTER_AREA)
-      test_image=np.array(test_image)
-      test_image=test_image/255
-      test_image=np.expand_dims(test_image, axis=0)
-      """
       if(type_=="mal" or type_=='brain'):
         test_image = image.load_img(name, target_size = (128, 128))
         test_image = image.img_to_array(test_image)
@@ -88,6 +124,13 @@ def index():
         test_image = test_image/255                                    #Scale the pixels between 0 and 1
         test_image = np.expand_dims(test_image, axis=0)                #Add another dimension because the model was trained on (n,128,128,3)
         data = test_image
+      
+      elif(type_=='fun'):
+        test_image = load_img(name, target_size=(224, 224))                          #Read image using the PIL library, Resize the images to 128x128 pixels
+        test_image = img_to_array(test_image)                                        #Conver the PIL image to numpy array
+        test_image = np.expand_dims(test_image, axis=0)                              #expand_dims will add an extra dimension to the data at a particular axis
+        test_image = vgg16.preprocess_input(test_image)                              #Prepare the image for the VGG model
+        data = test_image
 
 
       model=get_model(type_)[0]
@@ -106,7 +149,6 @@ def index():
                             "pred_val": pred_val})
 
       elif(type_=='breast'):
-         print("YESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS")
          preds, pred_val = translate_cancer(model["model"].predict_proba(data), model["type"])
          final_json.append({"empty": False, 
                             "type":model["type"], 
@@ -136,6 +178,16 @@ def index():
                             "norm":preds[2],
                             "severe":preds[3],
                             "pred_val": pred_val})
+      elif(type_=='fun'):
+         preds, pred_val = translate_vgg_16(model["model"].predict(data), model["type"])
+         final_json.append({"empty": False, 
+                            "type":model["type"], 
+                            "top1":preds[0], 
+                            "top2":preds[1],
+                            "top3":preds[2],
+                            "top4":preds[3],
+                            "top5":preds[4],
+                            "pred_val": pred_val})
     else:
       warn = "Feeding blank image won't work. Please enter an input image to continue."
       pred_val =" "
@@ -161,7 +213,6 @@ def index():
 def load_model_(model_name):
 	model_name = os.path.join("weights",model_name)
 	model = load_model(model_name)
-	print("Model {} successfuly loaded".format(model_name))
 	return model
 
 def get_model(name = None):
@@ -178,12 +229,12 @@ def get_model(name = None):
     model_name.append({"model": load_model_("diabetes_retinopathy.h5"), "type": name})
   elif(name=='breast'):
     model_name.append({"model": load_model_("breastcancer.h5"), "type": name})
+  elif(name=='fun'):
+    model_name.append({"model": load_vgg16_model(), "type": name})
+
   return model_name
 
 def translate_malaria(preds, type_):
-  print("Prediction Value")
-  print(preds)
-
   y_proba_Class0 = preds.flatten().tolist()[0] * 100
   y_proba_Class1 = 100.0-y_proba_Class0
 
@@ -197,7 +248,7 @@ def translate_malaria(preds, type_):
       prediction="Inference: The cell image shows strong evidence of Malaria."
       return total,prediction
   else:
-      prediction="Inference: The cell image shows no evidence Malaria."
+      prediction="Inference: The cell image shows no evidence of Malaria."
       return total,prediction
 
 def translate_cancer(preds, type_):
@@ -217,29 +268,22 @@ def translate_cancer(preds, type_):
       return total,prediction
 
 def translate_brain(preds, type_):
-  print("Prediction Value Brain")
-  print(preds)
-
   y_proba_Class0 = preds.flatten().tolist()[0] * 100
   y_proba_Class1 = 100.0-y_proba_Class0
 
   tumor="Probability of the MRI scan to have a brain tumor: {:.2f}%".format(y_proba_Class1)
   normal="Probability of the MRI scan to not have a brain tumor: {:.2f}%".format(y_proba_Class0)
 
-  #total = para_prob + " " + unifected_prob
   total = [tumor, normal]
 
   if (y_proba_Class1 > y_proba_Class0):
-      prediction="Inference: The MRI Scan has brain tumor."
+      prediction="Inference: The MRI scan has a brain tumor."
       return total,prediction
   else:
-      prediction="Inference: The MRI Scan does not have brain tumor."
+      prediction="Inference: The MRI scan does not show evidence of any brain tumor."
       return total,prediction
 
 def translate_oct(preds, type_):
-  print("Prediction Value OCT")
-  print(preds)
-
   y_proba_Class0 = preds.flatten().tolist()[0] * 100
   y_proba_Class1 = preds.flatten().tolist()[1] * 100
   y_proba_Class2 = preds.flatten().tolist()[2] * 100
@@ -253,10 +297,10 @@ def translate_oct(preds, type_):
   total = [cnv,dme,drusen,normal]
   
   list_proba = [y_proba_Class0,y_proba_Class1,y_proba_Class2,y_proba_Class3]
-  statements = ["The image has high evidence of Choroidal Neo Vascularization in the retinal pigment epithelium.",
-               "The image has high evidence of Diabetic Macular Edema in the retinal pigment epithelium.",
-               "The image has high evidence of Hard Drusen in the retinal pigment epithelium.",
-               "The retinal pigment epithelium layer looks absolutely normal."]
+  statements = ["Inference: The image has high evidence of Choroidal Neo Vascularization in the retinal pigment epithelium.",
+               "Inference: The image has high evidence of Diabetic Macular Edema in the retinal pigment epithelium.",
+               "Inference: The image has high evidence of Hard Drusen in the retinal pigment epithelium.",
+               "Inference: The retinal pigment epithelium layer looks absolutely normal."]
   
   
   index = list_proba.index(max(list_proba))
@@ -265,9 +309,6 @@ def translate_oct(preds, type_):
   return total, prediction
 
 def translate_pnm(preds, type_):
-  print("Prediction Value PNM")
-  print(preds)
-
   y_proba_Class0 = preds.flatten().tolist()[0] * 100
   y_proba_Class1 = preds.flatten().tolist()[1] * 100
   y_proba_Class2 = preds.flatten().tolist()[2] * 100
@@ -279,9 +320,9 @@ def translate_pnm(preds, type_):
   total = [bac,norm,viral]
 
   list_proba = [y_proba_Class0,y_proba_Class1,y_proba_Class2]
-  statements = ["The chest X-Ray has high evidence for the presence of Bacterial Pneumonia.",
-                "The chest X-Ray image is normal.",
-                "The chest X-Ray has high evidence for the presence of Viral Pneumonia."]
+  statements = ["Inference: The chest X-Ray image shows high evidence of Bacterial Pneumonia.",
+                "Inference: The chest X-Ray image is normal.",
+                "Inference: The chest X-Ray image shows high evidence of Viral Pneumonia."]
   
   index = list_proba.index(max(list_proba))
   prediction = statements[index]
@@ -289,9 +330,6 @@ def translate_pnm(preds, type_):
   return total, prediction
 
 def translate_retinopathy(preds, type_):
-  print("Prediction Value Retinopathy")
-  print(preds)
-
   y_proba_Class0 = preds.flatten().tolist()[0] * 100
   y_proba_Class1 = preds.flatten().tolist()[1] * 100
   y_proba_Class2 = preds.flatten().tolist()[2] * 100
@@ -305,10 +343,10 @@ def translate_retinopathy(preds, type_):
   total = [mild,mod,norm,severe]
   
   list_proba = [y_proba_Class0,y_proba_Class1,y_proba_Class2,y_proba_Class3]
-  statements = ["The image has high evidence for Mild Nonproliferative Diabetic Retinopathy Disease.",
-               "The image has high evidence for Moderate Nonproliferative Diabetic Retinopathy Disease.",
-               "The image has no evidence for Nonproliferative Diabetic Retinopathy Disease.",
-               "The image has high evidence for Severe Nonproliferative Diabetic Retinopathy Disease."]
+  statements = ["Inference: The image has high evidence for Mild Nonproliferative Diabetic Retinopathy Disease.",
+               "Inference: The image has high evidence for Moderate Nonproliferative Diabetic Retinopathy Disease.",
+               "Inference: The image has no evidence for Nonproliferative Diabetic Retinopathy Disease.",
+               "Inference: The image has high evidence for Severe Nonproliferative Diabetic Retinopathy Disease."]
   
   index = list_proba.index(max(list_proba))
   prediction = statements[index]
@@ -318,6 +356,37 @@ def translate_retinopathy(preds, type_):
 
 #Predict image using VGG16 pretrained models
 
+def translate_vgg_16(preds, type_):
+  label = decode_predictions(preds)
+  class_list = []
+  conf_list = []
+
+  for classes in label[0]:
+      class_list.append(classes[1])
+      conf_list.append(classes[2])
+
+  top1 = convert_results(class_list[0])
+  top2 = convert_results(class_list[1])
+  top3 = convert_results(class_list[2])
+  top4 = convert_results(class_list[3])
+  top5 = convert_results(class_list[4])
+
+  top1_proba = conf_list[0]
+  top2_proba = conf_list[1]
+  top3_proba = conf_list[2]
+  top4_proba = conf_list[3]
+  top5_proba = conf_list[4]
+
+  top1_sent = ["Probability of the input image to be {}: {:.2f}%".format(top1,top1_proba*100)]
+  top2_sent = ["Probability of the input image to be {}: {:.2f}%".format(top2,top2_proba*100)]
+  top3_sent = ["Probability of the input image to be {}: {:.2f}%".format(top3,top3_proba*100)]
+  top4_sent = ["Probability of the input image to be {}: {:.2f}%".format(top4,top4_proba*100)]
+  top5_sent = ["Probability of the input image to be {}: {:.2f}%".format(top5,top5_proba*100)]
+
+  total = [top1_sent,top2_sent,top3_sent,top4_sent,top5_sent]
+  prediction = ["Inference: The image is most likely to be of {}.".format(top1)]
+
+  return total, prediction
+
 
 app.run("0.0.0.0",80, debug = False)
-
