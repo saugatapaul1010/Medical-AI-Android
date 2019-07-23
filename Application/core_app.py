935ba3e924ea6c7a50363a82cba234e4d0dd6fdf
Saugata Paul <saugata.paul1010@gmail.com>
2019-07-23 18:22:54 +0530

    Update core

--- Application/core_app.py
+++ Application/core_app.py
@@ -41,6 +41,7 @@ def convert_results(string):
   name = name.title()
   return name
 
+
 #https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg16_weights_tf_dim_ordering_tf_kernels.h5
 def load_vgg16_model():
   input_shape = (224, 224, 3)
